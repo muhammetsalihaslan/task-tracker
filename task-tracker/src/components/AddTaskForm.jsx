7e5ddb020e88206265cccebb03f7ed032c661f05
salih <muhammetsalihaslan@gmail.com>
2022-09-06 18:26:01 +0300

    addtask

--- task-tracker/src/components/AddTaskForm.jsx
+++ task-tracker/src/components/AddTaskForm.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
 
 
 const AddTaskForm = () => {
+    const [task, setTask] = useState('');
+    const [day, setDay] = useState('');
     return (
         <form>
-
+         <div className="form-control" >
+            <label htmlFor="task">Task</label>
+            <input 
+            type="text"
+            name="task"
+            id="task"
+            placeholder="add task"
+            value={task} 
+            onChange={(e) => setTask(e.target.value)} 
+            required
+            />
+        </div>
+        <div className = "form-control">
+            <label htmlFor="day"> Day & Time</label>
+            <input 
+            type="date" 
+            name="day"
+            id="day"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+            required
+            />
+        </div>
         <div>
-           
+            <button
+            className="btn btn-submit"
+            type="submit">
+                Submit
+
+            </button>
         </div>
         </form>
     );
