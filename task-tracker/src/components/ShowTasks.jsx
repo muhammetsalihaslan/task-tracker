5f4e41c630f85364f009ae072fc5fe0db2034e15
salih <muhammetsalihaslan@gmail.com>
2022-09-13 15:27:00 +0300

    showtask

--- task-tracker/src/components/ShowTasks.jsx
+++ task-tracker/src/components/ShowTasks.jsx
@@ -1,18 +1,21 @@
-import { FaWindowClose } from 'react-icons/Fa';
+import {TiTrash} from "react-icons/ti";
+
 
 const ShowTasks = () => {
-  return (
-    <div className="st" >
-      <input 
-      className="show"
-      type="text"
-      name="task" 
-      id="task"
-      
-      />
+  return ( 
+    <div className="container">
+      <div key={1} className="btn-tasks" >
+        <div className="task-area">
+          <h3>yapılacak listesi</h3>
+          <p>05.01.2022</p>
+        </div>
+        <div className="btn-delete">
+          <TiTrash size={35}/>
+        </div>
+      </div>
 
     </div>
-  );
-};
+  )
+} 
 
 export default ShowTasks;
